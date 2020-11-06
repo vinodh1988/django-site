769a06f2b1393d092374ff020fa82889a5476484
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-06 11:49:11 +0530

    model-creation

--- mysite/applicants/models.py
+++ mysite/applicants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Applicants(models.Model):
+    fname = models.CharField("fname", max_length=255)
+    gender = models.CharField("gender", max_length=255)
+    city =  models.CharField("city", max_length=255)
+    
