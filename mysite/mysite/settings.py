f400ae5b0ffd844e1a589fc4fcc667b30acb8b41
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-06 10:28:37 +0530

    db-config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+    }    
 }
 
 
